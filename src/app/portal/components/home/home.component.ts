85081d176e3b86ddee3a69f0e770d6f753988afb
Jesus Lara <jesuslara97@gmail.com>
2020-05-27 11:48:48 -0500

    Mentoria 27 de Mayo

--- src/app/portal/components/home/home.component.ts
+++ src/app/portal/components/home/home.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { imagesArray } from './images';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    images: any = imagesArray;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void {}
 }
--- src/app/portal/portal.module.ts
+++ src/app/portal/portal.module.ts
@@ -6,6 +6,7 @@ import { PortalComponent } from './portal.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { UsComponent } from './components/us/us.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { UsComponent } from './components/us/us.component';
     PortalComponent,
     NavbarComponent,
     FooterComponent,
+    HomeComponent,
     UsComponent,
   ],
   imports: [
